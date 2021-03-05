ab8355e3fcd7c698b4c37e19b4ceccad477135be
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-03-06 02:14:24 +0900

    評価の比率を表すバーを非表示にする（厳密には「機能を殺す」）スクリプトを追加

---
+++ hide_sentiment_bar.js
@@ -0,0 +1,8 @@
+// 高評価ボタンの下にある評価比率バーを非表示にする（これが生きていると、低評価ボタンだけを非表示にしても場合によっては意味がない事があると思うので）
+// ※CSSでやりたいんだけど何故か上手く行かないのでJavaScriptで強引に対応。
+window.addEventListener("load", function ()
+{
+    var bar = document.getElementById("like-bar");
+    if (bar === undefined) { return; }
+    bar.style.display = "none";
+}, false);
