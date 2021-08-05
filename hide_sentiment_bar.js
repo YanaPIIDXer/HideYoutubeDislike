cb80ee18f5f02e84da4b860fbab4f759a59b5e91
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-08-05 18:18:07 +0900

    とりあえずsetIntervalに変えてみた ※何故か消えなくなってる

--- hide_sentiment_bar.js
+++ hide_sentiment_bar.js
@@ -1,11 +1,9 @@
 // 高評価ボタンの下にある評価比率バーを非表示にする（これが生きていると、低評価ボタンだけを非表示にしても場合によっては意味がない事があると思うので）
 // ※CSSでやりたいんだけど何故か上手く行かないのでJavaScriptで強引に対応。
-window.addEventListener("load", function ()
-{
-    setTimeout(function ()
-    {
+window.addEventListener("load", function () {
+    setInterval(() => {
         var bar = document.getElementById("sentiment");
         if (bar === undefined) { return; }
-        bar.style.display = "none";    
+        bar.style.display = "none";
     }, 500);
 }, false);
